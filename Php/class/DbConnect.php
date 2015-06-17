84e779927bff38fdef12c797be6c9f17bfbd4f9e
Kyz <loic.bouchisse@laposte.fr>
2015-06-17 12:20:08 +0200

    lol

---
+++ Php/class/DbConnect.php
@@ -0,0 +1,58 @@
+<?php 
+
+class dbConnect{
+
+	private $_dbConnect = null;
+	private $_host = "localhost";
+	private $_login = "root";
+	private $_password = "";
+	private $_dbname = "walley";
+	
+	function __construct()
+	{
+		$this->_dbConnect = new PDO("mysql:host=".$this->_host.";dbname=".$this->_dbname."", $this->_login, $this->_password);
+	}
+	
+	
+	/**
+	 * 
+	 */
+	function getHost(){
+		return $this->_host;
+	}
+	
+	
+	function setHost($host){
+		return $this->_host = $host;
+	}
+	
+	
+	
+	function GetUserByName($name, $pass)
+	{
+		$req = $this->_dbConnect->prepare ('SELECT * FROM utilisateur  WHERE name =:name');
+
+		$req->execute(
+			array(
+					'name' => $name
+				)
+			);
+			$response = $req->fetch();
+			var_dump($req);
+						
+			if ($response){
+						if ($response['pass'] == $pass)
+						{
+							echo "Bienvenue ".$response['name'];
+							//header("Location: acceuil.php");
+							
+						}else{
+							echo "Mauvais mot de passe";
+						}
+			}else{
+
+				echo "mauvais identifiant";
+			}
+	}
+
+}
--- Php/login.php
+++ Php/login.php
@@ -1,51 +1,9 @@
 <?php
 
-
-class dbConnect{
-
-	private $_name = $_GET['name'];
-	private $_pass = $_GET['pass'];
-	private $_dbConnect = null;
-	private $_host = "localhost";
-	private $_login = "root";
-	private $_$password = "";
-	private $_$dbname = "walley";
-	
-	function __construct(){
-		$_dbConnect = new PDO("mysql:host=".$host.";dbname=".$dbname."", $login, $password);
-		
-	
-	}
-	
-	fuction GetUserByName($name){
-		$req = $db->prepare ('SELECT * FROM utilisateur  WHERE name =:name');
-
-		$req->execute(
-			array(
-					'name' => $name
-				)
-			);
-			$response = $req->fetch();
-			var_dump($req);
-						
-			if ($response){
-						if ($response['pass'] == $pass)
-						{
-							echo "Bienvenue ".$response['name'];
-							//header("Location: acceuil.php");
-							
-						}else{
-							echo "Mauvais mot de passe";
-						}
-			}else{
-
-				echo "mauvais identifiant";
-			}
-	}
-
-}
+ include ("class/DbConnect.php");
 
  $db = new dbConnect();
- $db->GetUserByName();
+ 
+ $db->GetUserByName($_GET['name'],$_GET['pass']);
 
 ?>
